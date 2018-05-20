109e9e097f6be989fadc2a0bd397b3ba8f3a47b6
FatihBezir <fatihbezir@hotmail.com>
2018-05-20 19:26:46 +0300

    ListView Paging Csharp

---
+++ ListView Paging Csharp/ListView Paging Csharp/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ListView_Paging_Csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.FistPage = new System.Windows.Forms.Button();
+            this.Back = new System.Windows.Forms.Button();
+            this.Next = new System.Windows.Forms.Button();
+            this.LastPage = new System.Windows.Forms.Button();
+            this.TextPageNumber = new System.Windows.Forms.Label();
+            this.ShowAll = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 274);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // FistPage
+            // 
+            this.FistPage.Location = new System.Drawing.Point(74, 271);
+            this.FistPage.Name = "FistPage";
+            this.FistPage.Size = new System.Drawing.Size(75, 23);
+            this.FistPage.TabIndex = 2;
+            this.FistPage.Text = "FistPage";
+            this.FistPage.UseVisualStyleBackColor = true;
+            this.FistPage.Click += new System.EventHandler(this.FistPage_Click);
+            // 
+            // Back
+            // 
+            this.Back.Location = new System.Drawing.Point(156, 271);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(75, 23);
+            this.Back.TabIndex = 3;
+            this.Back.Text = "Back";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // Next
+            // 
+            this.Next.Location = new System.Drawing.Point(278, 271);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(75, 23);
+            this.Next.TabIndex = 4;
+            this.Next.Text = "Next";
+            this.Next.UseVisualStyleBackColor = true;
+            this.Next.Click += new System.EventHandler(this.Next_Click);
+            // 
+            // LastPage
+            // 
+            this.LastPage.Location = new System.Drawing.Point(359, 271);
+            this.LastPage.Name = "LastPage";
+            this.LastPage.Size = new System.Drawing.Size(75, 23);
+            this.LastPage.TabIndex = 5;
+            this.LastPage.Text = "LastPage";
+            this.LastPage.UseVisualStyleBackColor = true;
+            this.LastPage.Click += new System.EventHandler(this.LastPage_Click);
+            // 
+            // TextPageNumber
+            // 
+            this.TextPageNumber.AutoSize = true;
+            this.TextPageNumber.Location = new System.Drawing.Point(237, 276);
+            this.TextPageNumber.Name = "TextPageNumber";
+            this.TextPageNumber.Size = new System.Drawing.Size(24, 13);
+            this.TextPageNumber.TabIndex = 6;
+            this.TextPageNumber.Text = "1/1";
+            // 
+            // ShowAll
+            // 
+            this.ShowAll.Location = new System.Drawing.Point(440, 271);
+            this.ShowAll.Name = "ShowAll";
+            this.ShowAll.Size = new System.Drawing.Size(75, 23);
+            this.ShowAll.TabIndex = 7;
+            this.ShowAll.Text = "ShowAll";
+            this.ShowAll.UseVisualStyleBackColor = true;
+            this.ShowAll.Click += new System.EventHandler(this.ShowAll_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(503, 253);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 306);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.ShowAll);
+            this.Controls.Add(this.TextPageNumber);
+            this.Controls.Add(this.LastPage);
+            this.Controls.Add(this.Next);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.FistPage);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "ListView Paging";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button FistPage;
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.Button Next;
+        private System.Windows.Forms.Button LastPage;
+        private System.Windows.Forms.Label TextPageNumber;
+        private System.Windows.Forms.Button ShowAll;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ ListView Paging Csharp/ListView Paging Csharp/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ListView_Paging_Csharp
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection connection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=ki.accdb");
+        OleDbCommand komut = new OleDbCommand();
+        DataSet ds = new DataSet();
+
+        public static int MaxObj, RowCount, ResidualNumber, LastPageNumber, PageNumber, Start = 0;
+        public static int line, column;
+        public static string[,] data;
+
+        public void Datax()
+        {
+            line = listView1.Items.Count - 1;
+            column = listView1.Columns.Count - 1;
+
+            data = new string[line + 1, column + 1];
+
+            for (int i = 0; i <= line; i++)
+            {
+                for (int w = 0; w <= column; w++)
+                {
+                    data[i, w] = listView1.Items[i].SubItems[w].Text;
+                }
+            }
+        }
+
+        void pagelist()
+        {
+            titleslist();
+            int a = -1;
+            if (RowCount != Start + ResidualNumber)
+            {
+                for (int i = Start; i < MaxObj + Start; i++)
+                {
+                    a++;
+                    for (int w = 0; w <= column; w++)
+                    {
+
+                        if (w == 0)
+                        {
+                            listView1.Items.Add(data[i, w]);
+                        }
+                        else
+                        {
+                            listView1.Items[a].SubItems.Add(data[i, w]);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = Start; i < Start + ResidualNumber; i++)
+                {
+                    a++;
+                    for (int w = 0; w <= column; w++)
+                    {
+
+                        if (w == 0)
+                        {
+                            listView1.Items.Add(data[i, w]);
+                        }
+                        else
+                        {
+                            listView1.Items[a].SubItems.Add(data[i, w]);
+
+                        }
+                    }
+                }
+            }
+        }
+
+        void titleslist()
+        {
+            listView1.Clear();
+            listView1.Columns.Add("ID");
+            listView1.Columns.Add("Name");
+            listView1.Columns.Add("LastName");
+            listView1.Columns.Add("Age");
+        }
+
+        void list()
+        {
+
+            connection.Close();
+            connection.Open();
+            titleslist();
+
+            komut.Connection = connection;
+            komut.CommandText = "SELECT * FROM ki";
+            OleDbDataReader okuyucu = komut.ExecuteReader();
+            while (okuyucu.Read())
+            {
+                int count = listView1.Items.Count;
+                listView1.Items.Add(okuyucu["ID"].ToString());
+                listView1.Items[count].SubItems.Add(okuyucu["Name"].ToString());
+                listView1.Items[count].SubItems.Add(okuyucu["LastName"].ToString());
+                listView1.Items[count].SubItems.Add(okuyucu["Age"].ToString());
+            }
+
+            RowCount = listView1.Items.Count;
+
+            connection.Close();
+
+            Datax();
+        }
+
+        void connect()
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            else
+            {
+                connection.Close();
+                connection.Open();
+            }
+        }
+
+        void ReadPageNumber()
+        {
+            if (ResidualNumber == 0 || RowCount <= MaxObj)
+            {
+                TextPageNumber.Text = PageNumber + " / " + LastPageNumber;
+            }
+            else
+            {
+                TextPageNumber.Text = PageNumber + " / " + (LastPageNumber + 1);
+            }
+        }
+
+        private void FistPage_Click(object sender, EventArgs e)
+        {
+            PageNumber = 1;
+            ReadPageNumber();
+            Start = 0;
+            Back.Enabled = false;
+            FistPage.Enabled = false;
+            Next.Enabled = true;
+            LastPage.Enabled = true;
+            pagelist();
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            --PageNumber;
+            ReadPageNumber();
+            Start = Start - MaxObj;
+            Next.Enabled = true;
+            LastPage.Enabled = true;
+
+            if (Start <= 0)
+            {
+                Start = 0;
+                Back.Enabled = false;
+                FistPage.Enabled = false;
+            }
+            pagelist();
+        }
+
+        private void Next_Click(object sender, EventArgs e)
+        {
+            ++PageNumber;
+            ReadPageNumber();
+            Start = Start + MaxObj;
+            Back.Enabled = true;
+            FistPage.Enabled = true;
+
+            if (Start >= RowCount || RowCount <= Start + MaxObj)
+            {
+                Next.Enabled = false;
+                LastPage.Enabled = false;
+            }
+            pagelist();
+        }
+
+        private void LastPage_Click(object sender, EventArgs e)
+        {
+            if (ResidualNumber == 0)
+            {
+                Start = RowCount - MaxObj;
+                PageNumber = LastPageNumber;
+            }
+            else
+            {
+                Start = RowCount - ResidualNumber;
+                PageNumber = LastPageNumber + 1;
+            }
+
+            ReadPageNumber();
+
+            Back.Enabled = true;
+            FistPage.Enabled = true;
+            Next.Enabled = false;
+            LastPage.Enabled = false;
+            pagelist();
+        }
+
+        private void ShowAll_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = 0;
+            TextPageNumber.Text = "1 / 1";
+            connect();
+            list();
+            connection.Close();
+            Back.Enabled = true;
+            FistPage.Enabled = true;
+            Next.Enabled = true;
+            LastPage.Enabled = true;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+            MaxObj = (int)numericUpDown1.Value;
+
+            if (MaxObj >= RowCount)
+            {
+                numericUpDown1.Value = 0;
+            }
+
+            PageNumber = 1;
+            Start = 0;
+            if (MaxObj > 0)
+            {
+                LastPageNumber = RowCount / MaxObj;
+                ResidualNumber = RowCount % MaxObj;
+                ReadPageNumber();
+                ShowAll.Enabled = true;
+                Back.Enabled = false;
+                FistPage.Enabled = false;
+                Next.Enabled = true;
+                LastPage.Enabled = true;
+                pagelist();
+            }
+            else
+            {
+                ShowAll.Enabled = false;
+                Back.Enabled = false;
+                FistPage.Enabled = false;
+                Next.Enabled = false;
+                LastPage.Enabled = false;
+                TextPageNumber.Text = "1 / 1";
+                list();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                list();
+                ShowAll.Enabled = false;
+                Back.Enabled = false;
+                FistPage.Enabled = false;
+                Next.Enabled = false;
+                LastPage.Enabled = false;
+                TextPageNumber.Text = "1 / 1";
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show(hata.Message);
+                Application.Exit();
+            }
+        }
+    }
+}
